409148856a91dfd5cd7beee576c36b622aa88f59
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-02-26 10:05:32 -0300

    Update New Text Document.ps1

--- New Text Document.ps1
+++ New Text Document.ps1
@@ -1,8 +1,6 @@
-# Path to avgctl.exe
-$avgctlPath = "C:\Program Files\AVG\Antivirus\avgctl.exe"
+$avgExclusionsPath = "HKLM:\SOFTWARE\AVG\Antivirus\Exclusions\Global"
 
-# Add a file or folder to exclusions
-& "$avgctlPath" /ADDPATH="C:\Path\To\FileOrFolder"
-
-# Add a URL to exclusions
-& "$avgctlPath" /ADDURL="http://example.com"
+# Add a file or folder exclusion
+$fileOrFolderPath = "C:\temp"
+$exclusionName = "ExcludeFiles" # Unique name for the exclusion
+New-ItemProperty -Path $avgExclusionsPath -Name $exclusionName -Value $fileOrFolderPath -PropertyType String -Force
